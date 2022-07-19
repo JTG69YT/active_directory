114cd9de2eb839b21c49928550e6853f0fddc3ea
Jesse Keskelä <monkeydie_@hotmail.com>
2022-07-20 01:53:07 +0300

    Kirjoitettu random_domain.ps1 PowerShell-skripti.

---
+++ koodi/random_domain.ps1
@@ -0,0 +1,42 @@
+param( [Parameter(Mandatory=$true)] $OutputJSONFile )
+
+$group_names = [System.Collections.ArrayList](Get-Content "tiedot/group_names.txt")
+$first_names = [System.Collections.ArrayList](Get-Content "tiedot/first_names.txt")
+$last_names = [System.Collections.ArrayList](Get-Content "tiedot/last_names.txt")
+$passwords = [System.Collections.ArrayList](Get-Content "tiedot/passwords.txt")
+
+$groups = @()
+$users = @()
+
+$num_groups = 10
+for ( $i = 0; $i -lt $num_groups; $i++ ) {
+    $group_name = (Get-Random -InputObject $group_names)
+    $group = @{ "name" = "$group_name" }
+    $groups += $group
+    $group_names.Remove($group_name)
+}
+
+$num_users = 100
+for ( $i = 0; $i -lt $num_users; $i++ ) {
+    $first_name = (Get-Random -InputObject $first_names)
+    $last_name = (Get-Random -InputObject $last_names)
+    $password = (Get-Random -InputObject $passwords)
+
+    $new_user = @{
+        "name" = "$first_name $last_name"
+        "password" = "$password"
+        "groups" = (Get-Random -InputObject $groups).name
+    }
+
+    $users += $new_user
+
+    $first_names.Remove($first_name)
+    $last_names.Remove($last_name)
+    $passwords.Remove($password)
+}
+
+ConvertTo-Json -InputObject @{
+    "domain" = "wtf.com"
+    "groups" = $groups
+    "users" = $users
+} | Out-File $OutputJSONFile
